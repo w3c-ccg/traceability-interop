da323554c626066062b6b1630e6f596e0b3e6604
Orie Steele <orie@transmute.industries>
2021-02-19 08:39:09 -0600

    Add all reports json (#120)
    * Add all reports json
    
    * Add CI
    
    * Fix ci
    
    * Add test results to github pages

--- packages/vc-http-api-test-server/services/jest/testReporter.js
+++ packages/vc-http-api-test-server/services/jest/testReporter.js
@@ -1,15 +1,18 @@
 const MyCustomReporter = require("jest-html-reporters");
-const fs = require('fs');
-const { promises } = require('fs');
+const fs = require("fs");
+const { promises } = require("fs");
 
 module.exports = async (directory, name, results) => {
   if (!fs.existsSync(directory)) {
     await promises.mkdir(directory, { recursive: true });
   }
-  const reporter = new MyCustomReporter({}, {
-    filename: `${directory}/report.html`,
-    pageTitle: `${name} VC-HTTP-API Test Report`
-  });
-  
+  const reporter = new MyCustomReporter(
+    {},
+    {
+      filename: `${directory}/index.html`,
+      pageTitle: `${name} VC-HTTP-API Test Report`,
+    }
+  );
+
   await reporter.onRunComplete({}, results);
 };
--- packages/vc-http-api-test-server/services/jest/testRunner.js
+++ packages/vc-http-api-test-server/services/jest/testRunner.js
@@ -1,12 +1,16 @@
 const runJestCli = require("./cli");
-const generateTestReport = require('./testReporter');
+const generateTestReport = require("./testReporter");
 const extractTestSummary = require("./utilities");
+const fs = require("fs");
+const path = require("path");
+const slugify = require("slugify");
+const { promises } = require("fs");
 
-const fs = require('fs');
-const { promises } = require('fs');
-
-const TEST_RESULTS_DIRECTORY = 'test_results';
-const TEST_FILE_JSON = 'testResults.json';
+const TEST_RESULTS_DIRECTORY = path.resolve(
+  __dirname,
+  "../../../../docs/test-suite"
+);
+const TEST_FILE_JSON = "testResults.json";
 
 async function capture(fn, p) {
   const originalWrite = p.write;
@@ -33,28 +37,33 @@ module.exports = runTests = async (config) => {
     if (Array.isArray(config)) {
       await Promise.all(
         config.map(async (element) => {
-          const directory = `${TEST_RESULTS_DIRECTORY}/${element.name}`;
+          const directory = `${TEST_RESULTS_DIRECTORY}/${slugify(element.name, {
+            replacement: "-",
+            lower: true,
+          })}`;
           if (!fs.existsSync(directory)) {
             await promises.mkdir(directory, { recursive: true });
           }
-          
+
           const output = await runJestCli(element);
           await generateTestReport(directory, element.name, output.results);
           const summary = extractTestSummary(output.results);
-          console.log(summary);
+          // console.log(summary);
           await promises.writeFile(
             `${directory}/${TEST_FILE_JSON}`,
             JSON.stringify(summary, null, 2)
           );
           results.push({
             name: element.name,
-            results: summary
-          })
+            results: summary,
+          });
         })
       );
-    }
-    else {
-      const directory = `${TEST_RESULTS_DIRECTORY}/${config.name}`;
+    } else {
+      const directory = `${TEST_RESULTS_DIRECTORY}/${slugify(config.name, {
+        replacement: "-",
+        lower: true,
+      })}`;
       const output = await runJestCli(config);
       await generateTestReport(directory, config.name, output.results);
       const summary = extractTestSummary(output.results);
@@ -64,15 +73,20 @@ module.exports = runTests = async (config) => {
       );
       results.push({
         name: config.name,
-        results: summary
-      })
+        results: summary,
+      });
     }
   };
-  
+
   const capturedReport = await capture(run, process.stderr);
+
+  await promises.writeFile(
+    `${TEST_RESULTS_DIRECTORY}/${TEST_FILE_JSON}`,
+    JSON.stringify(results, null, 2)
+  );
 
   return {
     suitesReportTerminal: Buffer.from(capturedReport).toString("base64"),
-    suitesReportJson: results
+    suitesReportJson: results,
   };
 };
