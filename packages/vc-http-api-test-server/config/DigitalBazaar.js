e2e0bbacb0e0f8c5a82b5ac509c3741738d8eee5
Ganesh Annan <gannan08@users.noreply.github.com>
2021-03-03 12:55:27 -0500

    Update Digital Bazaar configuration. (#137)

--- packages/vc-http-api-test-server/config/DigitalBazaar.js
+++ packages/vc-http-api-test-server/config/DigitalBazaar.js
@@ -3,20 +3,22 @@ module.exports = {
     issueCredentialConfiguration: [
         {
             id: "did:key:z6Mkg9AkjefxdJFSphkStzXwHQnbweN43mCqA37aANGRxF1o",
-            endpoint: "https://issuer.interop.digitalbazaar.com/credentials/did%3Akey%3Az6MkkHSTSr9DSNLoioiVEZq8RKm9Sn1Xs4SjZXgzQASBMdc3/issueCredential",
+            endpoint: "https://issuer.demo.digitalbazaar.com/credentials/did%3Akey%3Az6MkmDoojMzE8aWK2a926xtVgBXY1GafcpnMSgGVaCdbAr3z/issueCredential",
             proofType: "Ed25519Signature2018",
             options: {
                 assertionMethod: "did:key:z6Mkg9AkjefxdJFSphkStzXwHQnbweN43mCqA37aANGRxF1o#z6Mkg9AkjefxdJFSphkStzXwHQnbweN43mCqA37aANGRxF1o"
-            }
+            },
+            credentialStatusesSupported: ['RevocationList2020Status']
         }
     ],
     verifyCredentialConfiguration: {
-        endpoint: "https://verifier.interop.digitalbazaar.com/verifier/credentials",
+        endpoint: "https://verifier.demo.digitalbazaar.com/verifier/credentials",
         didMethodsSupported: [ "did:key:", "did:v1:test:nym:" ],
-        linkedDataProofSuitesSupported: [ "Ed25519Signature2018" ]
+        linkedDataProofSuitesSupported: [ "Ed25519Signature2018" ],
+        credentialStatusesSupported: ['RevocationList2020Status']
     },
     verifyPresentationConfiguration: {
-        endpoint: "https://verifier.interop.digitalbazaar.com/verifier/presentations"
+        endpoint: "https://verifier.demo.digitalbazaar.com/verifier/presentations"
     },
     credentials: require('../__fixtures__/credentials'),
     verifiableCredentials: require('../__fixtures__/verifiableCredentials'),
