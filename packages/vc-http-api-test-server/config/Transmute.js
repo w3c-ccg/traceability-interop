c9d420d4916f99e887d309efaf1857ea104c31d4
Orie Steele <orie@transmute.industries>
2021-03-04 13:25:41 -0600

    Update Transmute config (#139)

--- packages/vc-http-api-test-server/config/Transmute.js
+++ packages/vc-http-api-test-server/config/Transmute.js
@@ -1,24 +1,25 @@
 module.exports = {
-    name: "Transmute",
-    issueCredentialConfiguration: [
-        {
-            id: "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
-            endpoint: "https://vc.transmute.world/v0.1.0/issue/credentials",
-            proofType: "Ed25519Signature2018",
-            options: {
-                assertionMethod:"did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
-            }
-        }
-    ],
-    verifyCredentialConfiguration: {
-        endpoint: "https://vc.transmute.world/v0.1.0/verify/credentials",
-        didMethodsSupported: [ "did:key:", "did:web:", "did:factom", "did:elem:ropsten:" ],
-        linkedDataProofSuitesSupported: [ "Ed25519Signature2018" ]
+  name: "Transmute",
+  issueCredentialConfiguration: [
+    {
+      id: "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
+      endpoint: "https://vc.transmute.world/next/credentials/issue",
+      proofType: "Ed25519Signature2018",
+      options: {
+        assertionMethod:
+          "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
+      },
     },
-    verifyPresentationConfiguration: {
-        endpoint: "https://vc.transmute.world/v0.1.0/verify/presentations"
-    },
-    credentials: require('../__fixtures__/credentials'),
-    verifiableCredentials: require('../__fixtures__/verifiableCredentials'),
-    verifiablePresentations: require('../__fixtures__/verifiablePresentations')
+  ],
+  verifyCredentialConfiguration: {
+    endpoint: "https://vc.transmute.world/next/credentials/verify",
+    didMethodsSupported: ["did:key:", "did:web:"],
+    linkedDataProofSuitesSupported: ["Ed25519Signature2018"],
+  },
+  verifyPresentationConfiguration: {
+    endpoint: "https://vc.transmute.world/next/presentations/verify",
+  },
+  credentials: require("../__fixtures__/credentials"),
+  verifiableCredentials: require("../__fixtures__/verifiableCredentials"),
+  verifiablePresentations: require("../__fixtures__/verifiablePresentations"),
 };
