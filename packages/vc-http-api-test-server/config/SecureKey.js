482b587639fb9895d2ea16995f6606da648c3a57
Firas Qutishat <firas.qutishat@securekey.com>
2021-03-12 19:22:55 -0500

    chore: update securekey config (#167)
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- packages/vc-http-api-test-server/config/SecureKey.js
+++ packages/vc-http-api-test-server/config/SecureKey.js
@@ -2,29 +2,32 @@ module.exports = {
     name: "SecureKey",
     issueCredentialConfiguration: [
         {
-            id: "did:trustbloc:testnet.trustbloc.dev:EiAFEDjO4aF0ItSmvK1ehKwtqyZlXExK8l70D6EP-TuF9A",
-            endpoint: "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop/credentials/issueCredential",
+            id: "did:trustbloc:testnet.trustbloc.dev:EiA93NYbmVnRB0vdlwYMijSoHDQL21QRo8bWCJ_h7rglGA",
+            endpoint: "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop/credentials/issue",
             proofType: "Ed25519Signature2018",
             options: {
-                assertionMethod: "did:trustbloc:testnet.trustbloc.dev:EiAFEDjO4aF0ItSmvK1ehKwtqyZlXExK8l70D6EP-TuF9A#cAl-0D3UEU2WOSlDrQXQWG2YQ51QIL6Ho8aeXaVzkfw"
-            }
+                assertionMethod: "did:trustbloc:testnet.trustbloc.dev:EiA93NYbmVnRB0vdlwYMijSoHDQL21QRo8bWCJ_h7rglGA#9b0aeZejuz3IFsDekhICFlGCBuN1HPsvYAVc-Yytw1s"
+            },
+            credentialStatusesSupported: ['RevocationList2020Status']
         },
         {
             id: "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
-            endpoint: "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop/credentials/issueCredential",
+            endpoint: "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop/credentials/issue",
             proofType: "Ed25519Signature2018",
             options: {
                 assertionMethod: "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
-            }
+            },
+            credentialStatusesSupported: ['RevocationList2020Status']
         }
     ],
     verifyCredentialConfiguration: {
-        endpoint: "https://verifier.sandbox.trustbloc.dev/verifier1/verifier/credentials",
+        endpoint: "https://verifier.sandbox.trustbloc.dev/vc-verifier-interop/verifier/credentials/verify",
         didMethodsSupported: [ "did:key:", "did:web:" ],
-        linkedDataProofSuitesSupported: [ "Ed25519Signature2018" ]
+        linkedDataProofSuitesSupported: [ "Ed25519Signature2018","BbsBlsSignatureProof2020"],
+        credentialStatusesSupported: [ "RevocationList2020Status"]
     },
     verifyPresentationConfiguration: {
-        endpoint: "https://verifier.sandbox.trustbloc.dev/verifier1/verifier/presentations"
+        endpoint: "https://verifier.sandbox.trustbloc.dev/vc-verifier-interop/verifier/presentations/verify"
     },
     credentials: require('../__fixtures__/credentials'),
     verifiableCredentials: require('../__fixtures__/verifiableCredentials'),
