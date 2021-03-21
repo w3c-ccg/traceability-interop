9f4be31e33cbcebaf48e0902715dc02ff94a1b36
Alexander Yenkalov <34243073+AlexYenkalov@users.noreply.github.com>
2021-03-21 19:58:48 +0200

    applying pre-prod tenants; adding RevocationList2020Status for credential issuance; (#169)

--- packages/vc-http-api-test-server/config/Spherity.js
+++ packages/vc-http-api-test-server/config/Spherity.js
@@ -1,34 +1,36 @@
 module.exports = {
-  name: 'Spherity',
-  issueCredentialConfiguration: [
-      {
-          id: "did:key:z6MkkvPAU8JWm65ktjSBjtGkdhAb1q71C255Dyq1A53CSQUF",
-          endpoint: "https://cbp-qa.api.wallet.spherity.io/api/v1/credential/issue",
-          proofType: "Ed25519Signature2018",
-          options: {
-              assertionMethod:
-                 "did:key:z6MkkvPAU8JWm65ktjSBjtGkdhAb1q71C255Dyq1A53CSQUF#z6MkkvPAU8JWm65ktjSBjtGkdhAb1q71C255Dyq1A53CSQUF"
-          }
-      },
-      {
-          id: "did:web:amazon-qa.wallet.spherity.io",
-          endpoint: "https://amazon-qa.api.wallet.spherity.io/api/v1/credential/issue",
-          proofType: "Ed25519Signature2018",
-          options: {
-              assertionMethod: 
-                "did:web:amazon-qa.wallet.spherity.io#z6MkpcgzRs3WffbyDs5NPwuEskwfJeTAsJmNBQBjJHHtK8ys"
-          }
-      },
-  ],
-  verifyCredentialConfiguration: {
-      endpoint: "https://amazon-qa.api.wallet.spherity.io/api/v1/credential/verify",
-      didMethodsSupported: [ "did:key:", "did:web:" ],
-      linkedDataProofSuitesSupported: [ "Ed25519Signature2018" ]
-  },
-  verifyPresentationConfiguration: {
-      endpoint: "https://amazon-qa.api.wallet.spherity.io/api/v1/presentation/verify",
-  },
-  credentials: require('../__fixtures__/credentials'),
-  verifiableCredentials: require('../__fixtures__/verifiableCredentials'),
-  verifiablePresentations: require('../__fixtures__/verifiablePresentations')
+    name: 'Spherity',
+    issueCredentialConfiguration: [
+        {
+            id: "did:key:z6MkjafApNWaUm4n7WHqYdjDwsjhKbZQi54LwPDb8U8fQAT9",
+            endpoint: "https://marketplace-amazon-pre-prod.api.wallet.eu.spherity.io/api/v1/vc-http-api/credentials/issue",
+            proofType: "Ed25519Signature2018",
+            options: {
+                assertionMethod:
+                   "did:key:z6MkjafApNWaUm4n7WHqYdjDwsjhKbZQi54LwPDb8U8fQAT9#z6MkjafApNWaUm4n7WHqYdjDwsjhKbZQi54LwPDb8U8fQAT9"
+            },
+            credentialStatusesSupported: ['RevocationList2020Status']
+        },
+        {
+            id: "did:key:z6MkjafApNWaUm4n7WHqYdjDwsjhKbZQi54LwPDb8U8fQAT9",
+            endpoint: "https://cbp-pre-prod.api.wallet.eu.spherity.io/api/v1/vc-http-api/credentials/issue",
+            proofType: "Ed25519Signature2018",
+            options: {
+                assertionMethod: 
+                  "did:key:z6MkemYya9bX1csgVvcjEaeLwSYufjnbe7746S4rsMfq3D2E#z6MkemYya9bX1csgVvcjEaeLwSYufjnbe7746S4rsMfq3D2E"
+            },
+            credentialStatusesSupported: ['RevocationList2020Status']
+        },
+    ],
+    verifyCredentialConfiguration: {
+        endpoint: "https://cbp-pre-prod.api.wallet.eu.spherity.io/api/v1/vc-http-api/credentials/verify",
+        didMethodsSupported: [ "did:key:", "did:web:" ],
+        linkedDataProofSuitesSupported: [ "Ed25519Signature2018" ],
+    },
+    verifyPresentationConfiguration: {
+        endpoint: "https://cbp-pre-prod.api.wallet.eu.spherity.io/api/v1/vc-http-api/presentations/verify",
+    },
+    credentials: require('../__fixtures__/credentials'),
+    verifiableCredentials: require('../__fixtures__/verifiableCredentials'),
+    verifiablePresentations: require('../__fixtures__/verifiablePresentations')
 };
