b0cd171515faae6ca126c94e309ce8735dca4225
Orie Steele <orie@transmute.industries>
2021-04-06 16:09:45 -0500

    Remove vc http api

--- vc-http-api-history/packages/vc-http-api-test-server/__fixtures__/verifiablePresentations/index.js
+++
@@ -1,9 +0,0 @@
-const fixtures = require('require-all')({
-    dirname: __dirname,
-    filter: /.json$/,
-    map: function (__, path) {
-        return `${path}`;
-    }
-})
-
-module.exports = Object.values(fixtures);
