555d306d5551c5110d7b0b3e31761788edb588f0
mprorock <mprorock@mesur.io>
2021-11-02 11:55:10 -0400

    feature: add interop tests from js

--- tests/postman/bin/index.js
+++ tests/postman/bin/index.js
@@ -1,3 +1,52 @@
 #!/usr/bin/env node
+const { program } = require('commander');
+var newman = require('newman');
 
-console.log('Beginning Traceability Interop Testing')
+console.log('Traceability Interop Testing')
+
+program.version('0.0.1');
+
+program
+    .option('-s, --service <file>', 'use the specified service provider test collection', './collections/service-providers.json');
+// program
+//     .option('-r, --reference <file>', 'use the specified reference VC test collection', './collections/reference-credentials.json')
+//     .option('-s, --service <file>', 'use the specified interop test collection', './collections/interop-credentials.json');
+
+program
+    .option('-sd, --servicedata <file>', 'use the specified service provider data collection', './data/service-providers.json');
+// program
+//     .option('-rd, --referencedata <file>', 'use the specified reference VC data collection', './data/reference-credentials.json')
+//     .option('-sd, --servicedata <file>', 'use the specified interop data collection', './data/interop-credentials.json');
+
+program
+    .option('-rd, --reportdir <folder>', 'use the specified service provider data collection', './newman');
+
+program
+    .option('-v, --verbose', 'verbose reporting', true);
+program
+    .option('-d, --dev', 'dev mode for advanced options', false);
+
+program.parse();
+
+var outputReporters = [ 'json', 'htmlextra' ]
+
+if (program.opts().verbose) {
+    outputReporters.push('cli');
+}
+if (program.opts().dev) {
+    console.log('*** DEV MODE SET ***');
+}
+
+
+newman.run({
+    collection: require(program.opts().service),
+    iterationData: require(program.opts().servicedata),
+    reporters: outputReporters,
+    reporter: {
+        json: { export: program.opts().reportdir+'/service-provider-report.json' },
+        htmlextra: { export: program.opts().reportdir+'/service-provider-report.html' }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('Traceability Interop test run complete!');
+});
