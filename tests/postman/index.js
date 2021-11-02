921eef2acfdfd67fe15d91fbbd563dc78bbcf0b5
mprorock <mprorock@mesur.io>
2021-11-02 13:33:03 -0400

    chore: add looping to service providers

--- tests/postman/index.js
+++ tests/postman/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-const { program } = require('commander');
+const { program , Option} = require('commander');
+const https = require('https');
 var newman = require('newman');
 
 console.log('Traceability Interop Testing')
@@ -20,10 +21,11 @@ program
 
 program
     .option('-rd, --reportdir <folder>', 'use the specified service provider data collection', './newman');
+program
+    .addOption(new Option('-t, --tests <tests...>', 'use the specified tests, "none" is provided as an option for dev purposes', ['all']).choices([ 'all', 'service', 'reference', 'interop', 'none' ]));
 
 program
-    .option('-v, --verbose', 'verbose reporting', true);
-program
+    .option('-v, --verbose', 'verbose reporting', true)
     .option('-d, --dev', 'dev mode for advanced options', false);
 
 program.parse();
@@ -37,31 +39,76 @@ if (program.opts().dev) {
     console.log('*** DEV MODE SET ***');
 }
 
+var serviceCollection = require(program.opts().service);
+var serviceData = require(program.opts().servicedata);
+
+// TODO: set reporter templates for htmlextra
 
 // first setup and run base service provider validation
-newman.run({
-    collection: require(program.opts().service),
-    iterationData: require(program.opts().servicedata),
-    reporters: outputReporters,
-    reporter: {
-        json: { export: program.opts().reportdir+'/service-provider-report.json' },
-        htmlextra: { export: program.opts().reportdir+'/service-provider-report.html' }
-    }
-}, function (err) {
-    if (err) { throw err; }
-    console.log('Traceability Interop: Service Provider test complete');
-});
+if (program.opts().tests.includes('all') || program.opts().tests.includes('service')) {
+    newman.run({
+        collection: serviceCollection,
+        iterationData: serviceData,
+        reporters: outputReporters,
+        reporter: {
+            json: { export: program.opts().reportdir+'/service-provider-report.json' },
+            htmlextra: { export: program.opts().reportdir+'/service-provider-report.html' }
+        }
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('Traceability Interop: Service Provider test complete');
+    });
+}
+
 
 // then run reference checks (this should loop each server from the service provider collection )
-newman.run({
-    collection: require(program.opts().reference),
-    iterationData: require(program.opts().referencedata),
-    reporters: outputReporters,
-    reporter: {
-        json: { export: program.opts().reportdir+'/reference-credentials-report.json' },
-        htmlextra: { export: program.opts().reportdir+'/reference-credentials-report.html' }
+// TODO: also loop dids
+if (program.opts().tests.includes('all') || program.opts().tests.includes('reference')) {
+    for (serviceIdx in serviceData) {
+        var serv = serviceData[serviceIdx];
+        console.log(serv);
+
+        //get did from .well-known
+        var didConfigURL = 'https://' + serv.serviceProvider.baseURL + '/.well-known/did-configuration.json';
+        https.get(didConfigURL, (res) => {
+            let body = "";
+        
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+        
+            res.on("end", () => {
+                try {
+                    let didConfig = JSON.parse(body);
+                    console.log(didConfig);
+                    for (didIdx in didConfig.linked_dids) {
+                        var did = didConfig.linked_dids[didIdx];
+                        newman.run({
+                            collection: require(program.opts().reference),
+                            iterationData: require(program.opts().referencedata),
+                            reporters: outputReporters,
+                            reporter: {
+                                json: { export: program.opts().reportdir+'/'+serv.serviceProvider.baseURL+'reference-credentials-report.json' },
+                                htmlextra: { export: program.opts().reportdir+'/'+serv.serviceProvider.baseURL+'reference-credentials-report.html' }
+                            },
+                            envVar: [
+                                { "key": "name", "value": serv.name},
+                                { "key": "server", "value": serv.serviceProvider.baseURL},
+                                { "key": "prefix", "value": serv.serviceProvider.vcPrefix},
+                                { "key": "did", "value": did.issuer},
+                            ]
+                        }, function (err) {
+                            if (err) { throw err; }
+                            console.log('Traceability Interop: Reference Credential test complete');
+                        });    
+                    }
+                } catch (error) {
+                    console.error(error.message);
+                };
+            });
+        
+        }).on("error", (error) => {
+            console.error(error.message);
+        });
     }
-}, function (err) {
-    if (err) { throw err; }
-    console.log('Traceability Interop: Reference Credential test complete');
-});
+}
