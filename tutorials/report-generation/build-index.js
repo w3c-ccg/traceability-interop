6dd0876bd2fa3575816980cad2aa8ca397dc6016
OR13 <OR13@users.noreply.github.com>
2022-04-02 15:36:19 +0000

    deploy: 2ca4ef3b885aca4e3aaf6ecd9c1f1b4cd5182fbe

--- tutorials/report-generation/build-index.js
+++ tutorials/report-generation/build-index.js
@@ -2,7 +2,7 @@
 /* eslint-disable function-paren-newline */
 const path = require('path');
 const fs = require('fs');
-
+const reportCleaner = require('./newman-json-sanitizer');
 const readFilesSync = (dir) => {
   const files = [];
   fs.readdirSync(dir).forEach((filename) => {
@@ -36,7 +36,11 @@ const readFilesSync = (dir) => {
 const cleanAndMoveReports = (relativePath) => {
   readFilesSync(path.join(process.cwd(), relativePath)).map((f) => {
     const cleanerName = (f.name + f.ext).replace('newman-run-report-', '');
-    fs.writeFileSync(path.join(__dirname, `../../reports/${cleanerName}`), f.content);
+    let { content } = f;
+    if (f.ext === '.json') {
+      content = JSON.stringify(reportCleaner(JSON.parse(content)), null, 2);
+    }
+    fs.writeFileSync(path.join(__dirname, `../../reports/${cleanerName}`), content);
     return cleanerName;
   });
 };
@@ -44,9 +48,7 @@ const cleanAndMoveReports = (relativePath) => {
 const buildReportsIndex = () => {
   const reports = readFilesSync(path.join(__dirname, '../../reports'))
     .filter((f) => f.name !== 'index' && ['.json', '.html'].includes(f.ext))
-    // blocked by https://github.com/perma-id/w3id.org/pull/2620
-    // .map((f) => `https://w3id.org/traceability/interoperability/reports/${f.name + f.ext}`);
-    .map((f) => `https://w3c-ccg.github.io/traceability-interop/reports/${f.name + f.ext}`);
+    .map((f) => `https://w3id.org/traceability/interoperability/reports/${f.name + f.ext}`);
 
   fs.writeFileSync(path.join(__dirname, '../../reports/index.json'), JSON.stringify({ items: reports }, null, 2));
 };
@@ -54,4 +56,5 @@ const buildReportsIndex = () => {
 (() => {
   cleanAndMoveReports('./newman');
   buildReportsIndex();
+  // consider removing reports older than 1 month / 1 year...
 })();
