504eb2afb508cd83f26d4be6b08afbeac6c362fe
Chris Abernethy <brownoxford@gmail.com>
2022-04-22 17:43:17 -0400

    fix: use correct syntax for reading env vars

--- test.js
+++ test.js
@@ -4,12 +4,12 @@ newman.run({
     collection: './docs/tutorials/presentations-verify/presentations-verify.postman_collection.json',
     reporters: ['json', 'htmlextra', 'cli'],
     environment: {
-      ORGANIZATION_DID_WEB: process.env('organization_did_web'),
-      CLIENT_ID: process.env('client_id'),
-      CLIENT_SECRET: process.env('client_secret'),
-      TOKEN_AUDIENCE: process.env('token_audience'),
-      TOKEN_ENDPOINT: process.env('token_endpoint'),
-      API_BASE_URL: process.env('api_base_url'),
+      ORGANIZATION_DID_WEB: process.env.organization_did_web,
+      CLIENT_ID: process.env.client_id,
+      CLIENT_SECRET: process.env.client_secret,
+      TOKEN_AUDIENCE: process.env.token_audience,
+      TOKEN_ENDPOINT: process.env.token_endpoint,
+      API_BASE_URL: process.env.api_base_url,
     }
 }, (err) => {
     if (err) { throw err; }
