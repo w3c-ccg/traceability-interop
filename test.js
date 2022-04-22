32c924fa256b4384de61ac20a80f8b833ffdc541
Chris Abernethy <brownoxford@gmail.com>
2022-04-22 17:40:49 -0400

    chore: try running as a library call via node

---
+++ test.js
@@ -0,0 +1,17 @@
+const newman = require('newman');
+
+newman.run({
+    collection: './docs/tutorials/presentations-verify/presentations-verify.postman_collection.json',
+    reporters: ['json', 'htmlextra', 'cli'],
+    environment: {
+      ORGANIZATION_DID_WEB: process.env('organization_did_web'),
+      CLIENT_ID: process.env('client_id'),
+      CLIENT_SECRET: process.env('client_secret'),
+      TOKEN_AUDIENCE: process.env('token_audience'),
+      TOKEN_ENDPOINT: process.env('token_endpoint'),
+      API_BASE_URL: process.env('api_base_url'),
+    }
+}, (err) => {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
