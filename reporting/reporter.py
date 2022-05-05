61baacb2d76f9f45b349b9f2d56d017ea8ef5a41
mprorock <mprorock@mesur.io>
2022-05-05 08:52:43 -0400

    fix: create data dir if not exists

--- reporting/reporter.py
+++ reporting/reporter.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import argparse
-from postman_reporter import report_data
+import os
+
 # from postman_reporter import report_static
 from postman_reporter import report_dashboard
+from postman_reporter import report_data
+
 
 def runData():
     report_data.getData()
     return 0
+
 
 def runDash():
     app = report_dashboard.getApp()
@@ -14,11 +18,20 @@ def runDash():
     app.run_server()
     return 0
 
+
 def runHtml():
-    print('Static HTML not yet implemented!')
+    print("Static HTML not yet implemented!")
     return 0
 
+
 def main(args):
+    # first check for data dir
+    if not os.path.exists("./data"):
+        os.mkdir(
+            "./data"
+        )  # explicitly let it throw an exception if permissions are not there
+
+    # now check args
     if args.mode == "all" or args.mode == "data":
         runData()
 
@@ -29,15 +42,18 @@ def main(args):
         runDash()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Interop test results reporting utility')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Interop test results reporting utility"
+    )
     parser.add_argument(
-        '--mode', type=str, 
+        "--mode",
+        type=str,
         default="all",
-        const='all', 
-        nargs='?',
-        choices=['all', 'data', 'html', 'dashboard'],
-        help='mode to run the reporter in'
+        const="all",
+        nargs="?",
+        choices=["all", "data", "html", "dashboard"],
+        help="mode to run the reporter in",
     )
 
     args = parser.parse_args()
